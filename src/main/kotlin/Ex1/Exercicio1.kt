f88411fd3670a3d6668ec07718afdf075c54c00b
Milena.Lopes.Mesquita.Alcântara <milena.alcantara@zup.com.br>
2023-06-30 11:34:51 -0300

    Atualização

--- src/main/kotlin/Ex1/Exercicio1.kt
+++ src/main/kotlin/Ex1/Exercicio1.kt
@@ -3,7 +3,6 @@ fun main(){
     println("Informe um número: ")
     println("O antecessor é: ${Exercicio1().mostrarAntecessor(readln().toInt())}")
 }
-
 class Exercicio1{
     fun mostrarAntecessor(numero : Int): Int {
         return numero -1
